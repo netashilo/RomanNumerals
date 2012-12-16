1c9c88ffbdabd967506c7a100dd9a9efabea80f4
Neta <netashilo@walla.com>
2012-12-16 12:37:33 +0200

    red

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -3,15 +3,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class RomanNumeralUnitTests {
+public class RomanNumeralUnitTests 
+{
 
 	private RomanNumerals number = new RomanNumerals();
 
 	@Test
-	public void testThat1ReturnsI() {
-		assertEquals("I", number.toRoman(1));
-		//assertEquals("II", number.toRoman(2));
+	public void test() 
+	{
+		assertEquals("MDIII", number.toRoman(103));
 	}
-	
-
 }
--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,10 +1,27 @@
 
-public class RomanNumerals {
+public class RomanNumerals 
+{
 
-	public String toRoman(int nmber) {
-		if (nmber == 1)
-			return "I";
-		return "II";
+	private final String[] RCODE = {"M", "CM", "D", "CD", "C", "XC", "L",
+										   "XL", "X", "IX", "V", "IV", "I"};
+	private final int[] 	  BVAL  = {1000, 900, 500, 400, 100, 90, 50,
+										   40,   10,   9,    5,   4,    1};
+	
+	public String toRoman(int num)
+	{
+		if(num <= 0 || num > 3000)
+			throw new NumberFormatException("Value outside roman numeral range.");
+	
+		String roman = "";
+		
+		for(int i=0; i < RCODE.length; i++)
+		{
+			while(num >= BVAL[i])
+			{
+				num -= BVAL[i];
+				roman += RCODE[i];
+			}
+		}
+		return roman;
 	}
-
 }
